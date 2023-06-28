0059bfc14a15c1d81cc4791b9fcff76139a65bf9
VinayShetty <134188405+VinayShettyJavaDev@users.noreply.github.com>
2023-06-28 10:00:20 +0530

    Update calculatorOperations.groovy
    updated the file

--- vars/calculatorOperations.groovy
+++ vars/calculatorOperations.groovy
@@ -1,1 +1,18 @@
+// calculator.groovy
+
+def add(int a, int b) {
+    return a + b
+}
+
+def subtract(int a, int b) {
+    return a - b
+}
+
+def multiply(int a, int b) {
+    return a * b
+}
+
+def divide(int a, int b) {
+    return a / b
+}
 
